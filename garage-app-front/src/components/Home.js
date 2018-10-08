681c3669ef32ec9fb7f4c2ed4afbd7fc9aa2f74d
FilipElen <filipelengit@outlook.com>
2018-10-08 11:19:30 +0200

    adds app-bar

--- garage-app-front/src/components/Home.js
+++ garage-app-front/src/components/Home.js
@@ -1,19 +1,22 @@
 import {Component} from "react";
 import logo from "../logo.svg";
 import React from "react";
+import DenseAppBar from "./DenseAppBar";
 
 class Home extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            <DenseAppBar>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </div>
+            </DenseAppBar>
         )
     }
 }
