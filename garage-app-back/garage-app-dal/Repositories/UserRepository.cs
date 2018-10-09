b84417a427f42fb4f2430fad634b1f8aeada95ba
FilipElen <filipelengit@outlook.com>
2018-10-09 15:45:58 +0200

    adds UserRepository

---
+++ garage-app-back/garage-app-dal/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using garage_app_entities;
+
+namespace DAL.Repositories
+{
+    class UserRepository
+    {
+        private MyDbContext _context;
+
+        public UserRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public User FindUser(int userId)
+        {
+            return _context.Users.Find(userId);
+        }
+
+        public User FindUserByUserName(string userName)
+        {
+            return _context.Users.First(user => user.UserName == userName);
+        }
+    }
+}
