3c84a8f79d18a7d343880fd099977948e87686e7
FilipElen <filipelengit@outlook.com>
2018-10-29 10:38:46 +0100

    adds new repositories for specifications

---
+++ garage-app-back/garage-app-dal/Repositories/SpecificationRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using garage_app_entities;
+
+namespace DAL.Repositories
+{
+    class SpecificationRepository
+    {
+        private readonly MyDbContext _context;
+
+        public SpecificationRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public void InsertSpecification(Specification specification)
+        {
+            _context.Specifications.Add(specification);
+            _context.SaveChanges();
+        }
+
+        public List<Specification> GetSpecifications()
+        {
+            return _context.Specifications.ToList();
+        }
+
+        public Specification FindSpecification(int id)
+        {
+            return _context.Specifications.Find(id);
+        }
+
+        public void UpdateSpecification(Specification specification)
+        {
+            _context.Specifications.Attach(specification);
+            _context.Entry(specification).State = EntityState.Modified;
+
+            _context.SpecificationTypes.Attach(specification.SpecificationType);
+
+            _context.SaveChanges();
+        }
+
+        public void DeleteSpecification(int id)
+        {
+            Specification specificationFromDb = _context.Specifications.Find(id);
+            if (specificationFromDb != null)
+            {
+                _context.Specifications.Remove(specificationFromDb);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new ArgumentException("specification was not found!");
+            }
+        }
+    }
+}
---
+++ garage-app-back/garage-app-dal/Repositories/SpecificationTypeRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using garage_app_entities;
+
+namespace DAL.Repositories
+{
+    public class SpecificationTypeRepository
+    {
+        private readonly MyDbContext _context;
+
+        public SpecificationTypeRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public void InsertSpecificationType(SpecificationType specificationType)
+        {
+            _context.SpecificationTypes.Add(specificationType);
+            _context.SaveChanges();
+        }
+
+        public List<SpecificationType> GetSpecificationTypes()
+        {
+            return _context.SpecificationTypes.ToList();
+        }
+
+        public SpecificationType FindSpecificationType(int id)
+        {
+            return _context.SpecificationTypes.Find(id);
+        }
+
+        public SpecificationType FindSpecificationType(string type)
+        {
+            SpecificationType specificationType = _context.SpecificationTypes.FirstOrDefault(c => c.Type.Equals(type));
+            if (specificationType != null)
+            {
+                return specificationType;
+            }
+            else
+            {
+                throw new ArgumentException($"specification type: {type} was not found");
+            }
+        }
+
+        public void UpdateSpecificationType(SpecificationType specificationType)
+        {
+            _context.SpecificationTypes.Attach(specificationType);
+            _context.Entry(specificationType).State = EntityState.Modified;
+
+            foreach (Specification specification in specificationType.Specifications)
+            {
+                _context.Specifications.Attach(specification);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void DeleteSpecificationType(int id)
+        {
+            SpecificationType specificationTypeFromDb = _context.SpecificationTypes.Find(id);
+            if (specificationTypeFromDb != null)
+            {
+                _context.SpecificationTypes.Remove(specificationTypeFromDb);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new ArgumentException("specificationType was not found!");
+            }
+        }
+    }
+}
