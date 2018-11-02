4e232426b902ac9802e9fca426120078dd0075ee
FilipElen <filipelengit@outlook.com>
2018-11-02 16:25:14 +0100

    move files from /Filter to /AuthFilter

--- garage-app-back/garage-app-service/AuthFilter/HttpAuthenticationChallengeContextExtensions.cs
+++ garage-app-back/garage-app-service/AuthFilter/HttpAuthenticationChallengeContextExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
-using System.Web;
 using System.Web.Http.Filters;
 
-namespace garage_app_service.Filter
+namespace garage_app_service.AuthFilter
 {
     public static class HttpAuthenticationChallengeContextExtensions
     {
